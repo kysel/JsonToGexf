a9dbf34d8c965c1ca02a854d290dc4667e5f633a
Jiří Kyzlink <jkyzlink@gmail.com>
2019-02-10 18:36:22 +0200

    Add project files.

---
+++ JsonToGexf/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace JsonToGexf
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
